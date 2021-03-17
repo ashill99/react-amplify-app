0d0f5c5eab46fd4d1a1ca23d42cbdb5d7f50bd8e
ashill99 <admshng@gmail.com>
2021-03-17 11:20:02 -0400

    added javatript vid

--- src/Pages/Projects/JavaTript.js
+++ src/Pages/Projects/JavaTript.js
@@ -11,10 +11,11 @@ return (
             <p >An Instagram-style photo journal that allows users to upload pictures from trips they have taken to a visual map, have them marked on the map with pins and allows users to like and comment on pictures </p>
             <ul>
                 <li>Built with Vanilla JavaScript front-end and Rails back-end</li>
-                <li>Uses Mapbox API for map and markers </li>
+                <li>Uses Mapbox API & Geocoder gem for map and markers </li>
             </ul>
         </div>
         <ul>
+        <iframe className="youtube-vid" width="560" height="315" src="https://www.youtube.com/embed/TT1q198Wv80" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><p></p>
             <img className="screenshots" src={javatriptHome} alt="JavaTript Home" />
             <img className="screenshots" src={javatriptTwo} alt ="JavaTript Main" />
         </ul>
